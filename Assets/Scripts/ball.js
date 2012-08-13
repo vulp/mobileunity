6ec53bf77a1b8b75ac300d5226202f22b62231a3
vetoketju <ville.valta@gmail.com>
2012-08-13 21:21:04 +0300

    Added collisioncheck and particles.

--- Assets/Scripts/ball.js
+++ Assets/Scripts/ball.js
@@ -9,11 +9,24 @@ function Start () {
 
 }
 
+function OnCollisionEnter(hit : Collision)
+{
+     if(hit.gameObject.tag == "boundsWall"){
+     	Debug.Log("Osui Seinään!");
+     }
+     else if(hit.gameObject.tag == "breakable"){
+     	Debug.Log("Osui Tiileen!");
+     }
+     else if(hit.gameObject.tag == "Player"){
+     	Debug.Log("Osui Mailaan!");
+     }
+}
+
 function Update () {
 	if(transform.position.y <= 295) Application.LoadLevel("mainmenu");
 }
 
 function increaseSpeed(){
 	rigidbody.velocity *=1.05;
-	Debug.Log("Velo = "+rigidbody.velocity);
+	//Debug.Log("Velo = "+rigidbody.velocity);
 }
